72df2d1f9301422ae9839d8c1ceacb2211065ee9
AyushTmg-3 <ayush.tamang@reviewsai.com.au>
2026-02-06 16:03:05 +0545

    Added learnings for Dependency Injection

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,59 @@
 package service
+
+import "github.com/gin-gonic/gin"
+
+// User represents a user in the system.
+type User struct {
+	ID       int
+	Username string
+}
+
+// UserService defines the interface for user-related operations.
+type UserService interface {
+	GetUserByID(id string) User
+	GetUserByUsername(username string) User
+}
+
+// BaseUserService is a basic implementation of the UserService interface.
+// in real applications, this would interact with a database.
+type BaseUserService struct {
+	UserData map[string]User
+}
+
+// GetUserByID retrieves a user by their ID.
+func (s *BaseUserService) GetUserByID(id string) User {
+	return s.UserData[id]
+}
+
+// GetUserByUsername retrieves a user by their username.
+func (s *BaseUserService) GetUserByUsername(username string) User {
+	for _, user := range s.UserData {
+		if user.Username == username {
+			return user
+		}
+	}
+	return User{}
+}
+
+// UserServiceHandler handles HTTP requests related to users.
+type UserServiceHandler struct {
+	Service UserService
+}
+
+// FetchUserByID handles a request to fetch a user by their ID.
+func (h *UserServiceHandler) FetchUserByID(c *gin.Context) {
+	id := c.Param("id")
+	c.JSON(
+		200,
+		h.Service.GetUserByID(id),
+	)
+}
+
+// FetchUserByUsername handles a request to fetch a user by their username.
+func (h *UserServiceHandler) FetchUserByUsername(c *gin.Context) {
+	username := c.Param("username")
+	c.JSON(
+		200,
+		h.Service.GetUserByUsername(username),
+	)
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"Gin/internal/handler"
+	"Gin/internal/service"
 	"Gin/middleware"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +11,28 @@ import (
 func main() {
 	app := gin.Default()
 	app.GET("/", handler.Home)
+
+	// static data
+	staticData := map[string]service.User{
+		"1": {ID: 1, Username: "Alice Smith"},
+		"2": {ID: 2, Username: "Bob Jones"},
+		"3": {ID: 3, Username: "Charlie Brown"},
+	}
+
+	// initialize the Base UserService with static data to simulate database interactions
+	userService := &service.BaseUserService{UserData: staticData}
+	userServiceHandler := &service.UserServiceHandler{Service: userService}
+
+	// define routes for fetching users by ID and username
+	/*
+		! Note:
+		the routes cannot be like user/:id and user/:username because they
+		will conflict with each other. So we need to differentiate them by adding
+		a prefix like in this case
+
+	*/
+	app.GET("users/id/:id", userServiceHandler.FetchUserByID)
+	app.GET("users/username/:username", userServiceHandler.FetchUserByUsername)
 
 	auth := app.Group("/auth", middleware.AuthMiddleware())
 	auth.POST("/register", handler.Register)
